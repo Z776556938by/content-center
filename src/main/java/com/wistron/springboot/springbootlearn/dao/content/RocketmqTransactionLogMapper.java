91425a1d15b8ae7d1749decd1d8b498fffa1252f
Lucas Y Zhang <k21064736@wistron.com>
2021-09-15 15:23:25 +0800

    update .gitignore

---
+++ src/main/java/com/wistron/springboot/springbootlearn/dao/content/RocketmqTransactionLogMapper.java
@@ -0,0 +1,7 @@
+package com.wistron.springboot.springbootlearn.dao.content;
+
+import com.wistron.springboot.springbootlearn.domain.entity.content.RocketmqTransactionLog;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface RocketmqTransactionLogMapper extends Mapper<RocketmqTransactionLog> {
+}
